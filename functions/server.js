6822b20fdce0b54b899854af7d4bbc3e8694aa70
Ehtisham <eht.elahi@gmail.com>
2023-06-08 13:28:31 -0400

    second commit1

--- functions/server.js
+++ functions/server.js
@@ -1,57 +1,50 @@
-exports.handler = (event, context, callback) => {
-  const express = require("express");
-  const serverless = require("serverless-http");
-  const cors = require("cors");
-  const path = require("path");
-  const bodyParser = require("body-parser");
-  const { Configuration, OpenAIApi } = require("openai");
-  require("dotenv").config();
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
 
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+let records = [];
 
-  const app = express();
-  const port = process.env.PORT || 3000;
+//Get all students
+router.get("/", (req, res) => {
+  res.send("App is running..");
+});
 
-  const static_path = path.join(__dirname, "public");
-  app.use(express.static(static_path));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(cors());
+//Create new record
+router.post("/add", (req, res) => {
+  res.send("New record added.");
+});
 
-  app.get("/", async (req, res) => {
-    res.json("App is running");
-  });
+//delete existing record
+router.delete("/", (req, res) => {
+  res.send("Deleted existing record");
+});
 
-  // Handling request
-  app.post("/prompt", async (req, res) => {
-    const response = await sendApiRequest(req.body.prompt);
-    res.json(response.data);
-  });
+//updating existing record
+router.put("/", (req, res) => {
+  res.send("Updating existing record");
+});
 
-  // Server Setup
-  app.listen(port, async () => {
-    console.log(`server is running at ${port}`);
-  });
+//showing demo records
+router.get("/demo", (req, res) => {
+  res.json([
+    {
+      id: "001",
+      name: "Smith",
+      email: "smith@gmail.com",
+    },
+    {
+      id: "002",
+      name: "Sam",
+      email: "sam@gmail.com",
+    },
+    {
+      id: "003",
+      name: "lily",
+      email: "lily@gmail.com",
+    },
+  ]);
+});
 
-  module.exports.handler = serverless(app);
-
-  // Send API Request
-  async function sendApiRequest(prompt) {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt:
-        "Act as an annoyed system administrator who likes to speak sarcastically. You will respond with vague unhelpful answers for every question." +
-        prompt,
-      temperature: 0.7,
-      max_tokens: 476,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-      stop: ["You:"],
-    });
-    return response;
-  }
-};
+app.use("/.netlify/functions/server", router);
+module.exports.handler = serverless(app);
